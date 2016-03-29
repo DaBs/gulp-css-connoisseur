a6e1d79682ff1cab420955552a5a68973aa96db6
David Alexander Bjerremose <dab@davidbjerremose.com>
2016-03-30 00:48:56 +0200

    Added media query functionality, updated dependencies

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ module.exports = function (opts) {
     opts.data = file.contents.toString();
     opts.css = css.parse(opts.data);
     opts.occurences = {};
+    opts.media = {};
     opts.postfix = opts.postfix || 'customize';
 
     if (opts.replaces) {
@@ -26,9 +27,37 @@ module.exports = function (opts) {
       });
     }
 
+    //console.log(opts.matches);
+
     try {
       opts.css.stylesheet.rules.forEach(function(rule) {
-        if(rule.declarations) {
+        var rules = [rule];
+        if (rule.type === 'media') {
+          var orgRule = rule;
+          var mediaVal = orgRule.media.replace(/ /g, "_");
+          orgRule.rules.forEach(function(rule) {
+            rule.declarations.forEach(function(declaration) {
+              opts.matches.forEach(function(match) {
+                if (declaration.value) {
+                  if (declaration.value.indexOf(match) > -1 && declaration.value.indexOf('gradient') < 0) {
+                    var val = declaration.value.replace(/ /g, "_");
+                    //console.log(declaration.value);
+                    //console.log(declaration);
+                    //console.log(rule);
+                    opts.media[mediaVal] = opts.media[mediaVal] || {};
+                    opts.media[mediaVal][val] = opts.media[mediaVal][val] || {};
+                    opts.media[mediaVal][val][declaration.property] = opts.media[mediaVal][val][declaration.property] || [];
+                    //console.log(opts.occurences[val][declaration.property]);
+                    rule.selectors.forEach(function(selector) {
+                      opts.media[mediaVal][val][declaration.property].push(selector);
+                    });
+                  };
+                };
+              });
+            });
+          });
+        }
+        if(rule.declarations && rule.type === 'rule') {
           rule.declarations.forEach(function(declaration) {
             opts.matches.forEach(function(match) {
               if (declaration.value) {
@@ -36,6 +65,7 @@ module.exports = function (opts) {
                   var val = declaration.value.replace(/ /g, "_");
                   //console.log(declaration.value);
                   //console.log(declaration);
+                  //console.log(rule);
                   opts.occurences[val] = opts.occurences[val] || {};
                   opts.occurences[val][declaration.property] = opts.occurences[val][declaration.property] || [];
                   //console.log(opts.occurences[val][declaration.property]);
@@ -83,6 +113,44 @@ module.exports = function (opts) {
         newCss.stylesheet.rules.push(rule);
       }
     }
+    for (var media in opts.media) {
+      var mediaRules = [];
+      var mediaVal = opts.media[media];
+      for (var key in mediaVal) {
+        var val = mediaVal[key];
+        for (var key2 in val) {
+          var val2 = val[key2];
+          var declarationValue = key.replace(/_/g, " ");
+          if (opts.replaces) {
+            Object.keys(opts.replaces).forEach(function(replaceKey) {
+              if (declarationValue.indexOf(replaceKey) > -1) {
+                console.log(declarationValue);
+                declarationValue = declarationValue.replace(replaceKey, opts.replaces[replaceKey]);
+              }
+            });
+          }
+          var rule = {
+            type: "rule",
+            selectors: val2,
+            declarations: [
+              {
+                type: "declaration",
+                property: key2,
+                value: declarationValue
+              }
+            ]
+          }
+          mediaRules.push(rule);
+        }
+      }
+      var mediaRule = {
+        type: "media",
+        media: media.replace(/_/g, " "),
+        rules: mediaRules
+      }
+      newCss.stylesheet.rules.push(mediaRule);
+    }
+    console.log(newCss);
     var string = css.stringify(newCss, {
       indent: opts.indent,
       compress: opts.compress,
